a92e52a3accd5e0737ec09311ebea55216270aa6
Monica <monica@Monicas-MacBook.local>
2020-05-06 20:48:14 +0100

    use provided static files

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './static/index.html'));
 });
