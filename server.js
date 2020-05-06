3c3be0b638407436f3714539c73fefdf184cf569
Monica <monica@Monicas-MacBook.local>
2020-05-06 20:44:37 +0100

    serve provided html

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello express');
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
