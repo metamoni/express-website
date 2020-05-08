f9e7220fb68267096fe6a21c62dce2b057c212a9
Monica <monica@monicas-macbook.home>
2020-05-08 07:49:16 +0100

    use provided feedback and speaker services

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
 const routes = require('./routes');
 
 const app = express();
@@ -11,7 +17,10 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+app.use('/', routes({
+  feedbackService,
+  speakerService
+}));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
