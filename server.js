988df4c19765b3449331fa1fc2d999b52e3df864
Monica <monica@monicas-macbook.home>
2020-05-08 07:57:21 +0100

    install and use cookie-session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
@@ -12,15 +13,27 @@ const routes = require('./routes');
 const app = express();
 const port = 3000;
 
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['OjaheRnsshemfnahELF08AEBsjh', 'iajeLSEbdjahwoPEs'],
+  })
+);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes({
-  feedbackService,
-  speakerService
-}));
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
