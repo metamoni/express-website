8254813107fe1284b6d3ed1adbd668c569b9de29
Monica <monica@monicas-macbook.home>
2020-05-30 09:57:06 +0100

    validate and sanitize form data

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 module.exports = (params) => {
@@ -8,19 +10,46 @@ module.exports = (params) => {
   router.get('/', async (req, res, next) => {
     try {
       const feedback = await feedbackService.getList();
+
+      const errors = req.session.feedback ? req.session.feedback.errors : false;
+      req.session.feedback = {};
+
       return res.render('layout', {
         pageTitle: 'Feedback',
         template: 'feedback',
         feedback,
+        errors,
       });
     } catch (err) {
       return next(err);
     }
   });
 
-  router.post('/', (req, res) => {
-    return res.send(`Feedback form posted`);
-  });
+  router.post(
+    '/',
+    [
+      check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+      check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('A valid email address is required'),
+      check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+      check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        req.session.feedback = {
+          errors: errors.array(),
+        };
+        return res.redirect('/feedback');
+      }
+
+      return res.send('Feedback posted');
+    }
+  );
 
   return router;
 };
